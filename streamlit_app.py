734410bc6bfdd94e8b13365413f8794be9878bfe
Ray Fevrier <raymond.fevrier@frfcg.com>
2023-06-10 23:56:27 -0400

    New Section to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,12 @@ streamlit.dataframe(fruits_to_show)
 
 #  Call the Fruityvice API from Streamlit App!
 
-# Testing API call
+# # Testing API call
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response)
+
+#New Section to display fruityvice api response
+streamlit.header ('Fruityvice Fruit Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
