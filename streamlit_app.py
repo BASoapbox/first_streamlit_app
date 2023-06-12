751e4a6c3ccca19f892b2b0aa32a8c94d4080a16
Ray Fevrier <raymond.fevrier@frfcg.com>
2023-06-12 03:29:31 -0400

    nm

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,16 @@ streamlit.dataframe(fruits_to_show)
 ### Call the Fruityvice API from Streamlit App!
 ### New Section to display fruityvice api response
 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    # API call and assigning response to a variable
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)    
+    
+    # Use Pandas to normalize json respomse
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    
+    return fruityvice_normalized 
+
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     ## Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
@@ -55,17 +65,12 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        # API call and assigning response to a variable
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        
-        # Use Pandas to normalize json respomse
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-        # asking streamlit library to display it on the page as DataFrame/table
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe (back_from_function)
 
 except URLError as e:
     streamlit.error()
+
 
 
 # Add a STOP Command to Focus Our Attention
