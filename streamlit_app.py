7cd6660d1d5954c14aa1c99c68a3533e71cd8935
Ray Fevrier <raymond.fevrier@frfcg.com>
2023-06-12 01:21:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,4 +100,18 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 
+# Let's Change the Streamlit Components to Make Things Look a Little Nicer
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
+
+
+# Let's Get All the Rows, Not Just One
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall() 
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+                                                     
+                                                     
+
 
